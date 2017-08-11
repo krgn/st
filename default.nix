ca0ea12fef0b3b266ab989b21e15b07dfb3c9056
Karsten Gebbert <k@ioctl.it>
2017-08-11 23:31:40 +0200

    add default.nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+with xorg;
+
+stdenv.mkDerivation {
+  name = "st-0.7ish";
+  buildInputs = [ pkgconfig libX11 ncurses libXext libXft fontconfig ];
+  src = ./.;
+}
